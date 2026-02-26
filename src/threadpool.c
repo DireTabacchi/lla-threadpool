b940f926f634985e6132a189b618b0ed20df8e7c
Nathaniel Tabacchi <ntabacchi@outlook.com>
2026-02-26 00:50:03 -0500

    Created threadpool structures.

---
+++ src/threadpool.c
@@ -0,0 +1,24 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "threadpool.h"
+
+void* thread_function(void* varg) {
+    return NULL;
+}
+
+void threadpool_init(threadpool_t *pool) {
+    pool->queued = 0;
+    pool->queue_front = 0;
+    pool->queue_back = 0;
+    pool->stop = 0;
+
+    pthread_mutex_init(&(pool->lock), NULL);
+    pthread_cond_init(&(pool->notify), NULL);
+
+    for (int i = 0; i < THREADS; i++) {
+        pthread_create(&(pool->threads[i]), NULL, thread_function, pool);
+    }
+}
